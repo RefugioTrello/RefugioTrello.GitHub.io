784e61a33eae5778a69f5bc0ea35573044a24579
IsmaCar <carballomartinismael@gmail.com>
2024-03-15 19:57:25 +0100

    add Description dogs

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ $(document).on("click", '[data-toggle="lightbox"]', function(event) {
 function dafne() {
   alert("Dafne es super cariñosa y no piensa perder la oportunidad de recibir mimos. Es super sociable con otros lobitos y lleva mucho tiempo esperando encontrar la familia que le de una segunda oportunidad. Como muchos otros, este tipo de perros son criados para caza y luego abandonados, pero este caso sorprende por su capicidad para vivir en una casita y ser un gran perrito de compañia!");
 }
+
+function Kala() {
+  alert("Kala Tamaño: Grande. Kala es la bondad perrificada. Es super tranquila y le encanta pasear. Su hobby favorito es tumbarse con los rayos de sol en la cara. No es nada agresiva con otros perros y es super obediente y respetuosa.");
+}  
